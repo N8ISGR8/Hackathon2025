8df21fca5c835320bb7e8f49e7ae3a28d7d01bdb
N8ISGR8 <39780901+N8ISGR8@users.noreply.github.com>
2025-02-15 20:38:29 -0800

    uhm i changed to vcpkg

--- main.cpp
+++ main.cpp
@@ -1,42 +1,40 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::document;
+using bsoncxx::builder::basic::array;
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 1200,900 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    // Load a sprite to display
-    const sf::Texture texture("Assets/cute_image.jpg");
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    const sf::Font font("Assets/ARIAL.TTF");
-    sf::Text text(font, "Hello SFML", 50);
-    text.setFillColor(sf::Color::Black);
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        while (const std::optional event = window.pollEvent())
-        {
-            // Close window: exit
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
+    mongocxx::instance instance;
+    mongocxx::uri uri("mongodb+srv://app_user:ex17OefiFYvMfKM3@cluster0.sz37p.mongodb.net/?retryWrites=true&w=majority");
+    mongocxx::client client(uri);
+    auto db = client["ArtDB"];
+    auto collection = db["Art"];
+    document doc = document{};
+    array arrTags = array{};
+    arrTags.append("tag1");
+    arrTags.append("tag2");
+    arrTags.append("tag3");
+    array arrChildren = array{};
+    arrTags.append(0);
+    arrTags.append(1);
+    arrTags.append(2);
+    doc.append(kvp("name", "test"));
+    doc.append(kvp("id", 2));
+    doc.append(kvp("parent", 0));
+    doc.append(kvp("tags", arrTags));
+    doc.append(kvp("children", arrChildren));
+    doc.append(kvp("data", "balls"));
+    
+    collection.insert_one(doc.view());
     return 0;
 }
