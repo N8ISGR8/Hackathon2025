ef20abaecb623b0fc29143fe285565e97621ff19
N8ISGR8 <39780901+N8ISGR8@users.noreply.github.com>
2025-02-16 01:52:23 -0800

    ggg

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 #include <vector>
 #include "DBHandler.h"
 #include <Windows.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 const char* test = "go fuck yourself";
 int main()
 {
+    sf::Window(sf::VideoMode({200,200}), "Name", sf::State::Windowed);
     MongoDB::SendImage("testname", 0, {"tag1", "tag2"}, test);
     Sleep(1000);
-    std::cout <<  MongoDB::ReadImageFromId(198424) << std::endl;
+    std::cout << MongoDB::ReadImageFromId(198424) << std::endl;
+    while (1);
     return 0;
 }
